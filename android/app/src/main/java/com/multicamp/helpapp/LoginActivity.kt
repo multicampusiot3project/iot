0b41c69f0df0a00e9dacd12d8e2af62a29b4953c
JinU(지누) <73889507+jinu12@users.noreply.github.com>
2022-06-08 14:16:23 +0900

    add login -- 로그인 기능 추가 및 네트워크 접속 권한 허가

---
+++ android/app/src/main/java/com/multicamp/helpapp/LoginActivity.kt
@@ -0,0 +1,58 @@
+package com.multicamp.helpapp
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.os.Looper
+import android.util.AttributeSet
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
+import okhttp3.*
+import org.eclipse.paho.android.service.MqttAndroidClient
+import org.eclipse.paho.client.mqttv3.*
+import org.json.JSONObject
+import kotlin.concurrent.thread
+
+class LoginActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        loginSubmit.setOnClickListener {
+            thread{
+                var jsonobj=JSONObject()
+                jsonobj.put("id",loginID.text)
+                jsonobj.put("password",loginPW.text)
+                val client=OkHttpClient()
+                val jsondata=jsonobj.toString()
+                val builder= Request.Builder()
+                val url="http://192.168.0.5:8000/loginandroid"
+                val nextIntent= Intent(this,HomeActivity::class.java)
+                builder.url(url)
+                builder.post(RequestBody.create(MediaType.parse("application/json"),jsondata))
+                val myrequest:Request=builder.build()
+                val response:Response=client.newCall(myrequest).execute()
+                var result:String?=response.body()?.string()
+                print(result)
+                Log.d("test",result+"test result 11111111111111111111111111111111111111")
+                result = result?.replace("\""," ")?.trim()
+                if(result=="ok"){
+                    Log.d("test",result!!+"ok result here!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+                    startActivity(nextIntent)
+                }else{
+                    Log.d("test","else result here !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+                    runOnUiThread {Toast.makeText(this,"로그인 실패!!",Toast.LENGTH_SHORT).show()}
+                }
+                Log.d("test",result!!+"result here!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+            }
+        }
+    }
+    fun onReceived(topic:String,message:MqttMessage){
+        val msg=String(message.payload)
+        Log.d("mymqtt","onReceived message 1111111111111")
+
+    }
+}
