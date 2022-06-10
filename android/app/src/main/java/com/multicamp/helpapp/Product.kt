97d122defecb8753ebdbae754f513befcd5ca6d0
JinU(지누) <73889507+jinu12@users.noreply.github.com>
2022-06-10 11:24:02 +0900

    finish picture function-- 사진 촬영 완료

--- android/app/src/main/java/com/multicamp/helpapp/Product.kt
+++ android/app/src/main/java/com/multicamp/helpapp/Product.kt
@@ -1,6 +1,10 @@
 package com.multicamp.helpapp
 
+import android.Manifest
+import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
@@ -8,103 +12,113 @@ import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
 import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
 import kotlinx.android.synthetic.main.activity_product.*
 import java.io.File
+import java.io.FileOutputStream
 import java.io.IOException
+import java.io.OutputStream
 import java.text.SimpleDateFormat
 import java.util.*
 
 class Product : AppCompatActivity() {
-    val sub_topic = "android/#"
-    val server_uri ="tcp://13.52.187.248" //broker의 ip와 port
-    var mymqtt : MyMqtt? = null
 
-    private val REQUEST_TAKE_PHOTO = 1
-    private lateinit var currentPhotoPath: String
+    // ViewBinding
+    // Permisisons
+    val PERMISSIONS = arrayOf(
+        Manifest.permission.CAMERA,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        Manifest.permission.READ_EXTERNAL_STORAGE    )
+    val PERMISSIONS_REQUEST = 100
+    // Request Code
+    private val BUTTON2 = 200
+    private val BUTTON3 = 300
+    private val BUTTON4 = 400
+    private val BUTTON5 = 500
+    // 원본 사진이 저장되는 Uri    private var photoUri: Uri? = null
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product)
 
-        takePictureIntent()
+        checkPermissions(PERMISSIONS, PERMISSIONS_REQUEST)
+
+        btn2.setOnClickListener {
+            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                startActivityForResult(takePictureIntent, BUTTON2)
+            }
+
+
+        }
+
+
     }
 
-    // 사진 찍는 인텐트
-    private fun takePictureIntent() {
-        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
-            takePictureIntent.resolveActivity(packageManager)?.also {
-                // 사진 파일을 만듭니다.
-                val photoFile: File? = try {
-                    createImageFile()
-                } catch (ex: IOException) {
-                    // Error occurred while creating the File
-                    Log.d("test", "error: $ex")
-                    null
+    private fun checkPermissions(permissions: Array<String>, permissionsRequest: Int): Boolean {
+        val permissionList : MutableList<String> = mutableListOf()
+        for(permission in permissions){
+            val result = ContextCompat.checkSelfPermission(this, permission)
+            if(result != PackageManager.PERMISSION_GRANTED){
+                    permissionList.add(permission)
                 }
-                // photoUri를 보내는 코드
-                photoFile?.also {
-                    val photoURI: Uri = FileProvider.getUriForFile(
-                        this,
-                        "com.multicamp.helpapp",
-                        it
-                    )
-                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
-                    startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO)
+            }
+        if(permissionList.isNotEmpty()){
+            ActivityCompat.requestPermissions(this, permissionList.toTypedArray(), PERMISSIONS_REQUEST)
+            return false
+        }
+        return true
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        for(result in grantResults){
+            if(result != PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this, "권한 승인 부탁드립니다.", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == Activity.RESULT_OK) {
+            when(requestCode) {
+                BUTTON2-> {
+                    val imageBitmap = data?.extras?.get("data") as Bitmap
+                    saveBitmapAsJPGFile(imageBitmap)
+                    views.setImageBitmap(imageBitmap)
                 }
             }
         }
     }
 
+    private fun newJpgFileName() : String {
+        val sdf = SimpleDateFormat("yyyyMMdd_HHmmss")
+        val filename = sdf.format(System.currentTimeMillis())
+        return "${filename}.jpg"} private fun saveBitmapAsJPGFile(bitmap: Bitmap) {
+        val path = File(filesDir, "image")
+        if(!path.exists()){
+            path.mkdirs()
+        }
+        val file = File(path, newJpgFileName())
+        var imageFile: OutputStream? = null
+        try{        file.createNewFile()
+            imageFile = FileOutputStream(file)
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, imageFile)
+            imageFile.close()
+            Toast.makeText(this, file.absolutePath, Toast.LENGTH_LONG).show()
+        }catch (e: Exception){
+            null
+        }}
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
-            galleryAddPic()
-            setPic()
-        }
-    }
-    // 사진 파일을 만드는 메소드
-    @Throws(IOException::class)
-    fun createImageFile(): File {
-        // Create an image file name
-        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
-        val storageDir: File? = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
-        return File.createTempFile(
-            "JPEG_${timeStamp}_", /* prefix */
-            ".jpg", /* suffix */
-            storageDir /* directory */
-        ).apply {
-            // Save a file: path for use with ACTION_VIEW intents
-            currentPhotoPath = absolutePath
-            Log.d("test", "currentPhotoPath : $currentPhotoPath")
-        }
-    }
-    // 갤러리에 파일을 추가하는 함수.
-    fun galleryAddPic() {
-        Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE).also { mediaScanIntent ->
-            Log.d("test", "currentPhotoPath2 : $currentPhotoPath")
-            val f = File(currentPhotoPath)
-            mediaScanIntent.data = Uri.fromFile(f)
-            sendBroadcast(mediaScanIntent)
-        }
-    }
-    // ImageView에 사진을 넣는 메소드
-    fun setPic() {
-        val targetW: Int = imageView.width
-        val targetH: Int = imageView.height
-        val bmOptions = BitmapFactory.Options().apply {
-            inJustDecodeBounds = true
-            val photoW: Int = outWidth
-            val photoH: Int = outHeight
-            // 이미지 배율 변수
-            val scaleFactor: Int = Math.min(photoW / targetW, photoH / targetH)
-            inJustDecodeBounds = false
-            inSampleSize = scaleFactor
-        }
-        BitmapFactory.decodeFile(currentPhotoPath, bmOptions)?.also { bitmap ->
-            imageView.setImageBitmap(bitmap)
-        }
-    }
+
+
 
 }
